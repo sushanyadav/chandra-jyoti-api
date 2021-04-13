a464b568a8ef032d29a790860f36b7fd64c9d2aa
Sushan <sushanyadav98@gmail.com>
2021-04-13 15:25:03 +0545

    init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0234dade58bd69327c13a9a065ab27bc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c9b24d8f-25a6-4a1f-b50d-a9d07c3384d3'
+};
